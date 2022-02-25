e5bc3ea58d3b46bf61fb665bcd10df31cca36c7d
Henrique Cocito <henriquecocito@gmail.com>
2022-02-25 15:54:23 -0300

    test: Adding corner cases on get user use case test

--- src/test/kotlin/com/henriquecocito/intellum/useCases/GetUserFromTextUseCaseTest.kt
+++ src/test/kotlin/com/henriquecocito/intellum/useCases/GetUserFromTextUseCaseTest.kt
@@ -11,13 +11,43 @@ class GetUserFromTextUseCaseTest {
     private val getUserFromTextUseCase: GetUserFromTextUseCase = GetUserFromText()
 
     @Test
-    fun shouldReturnAListOfUsers_whenTextIsGiven() {
+    fun shouldReturnASetOfUsers_whenTextIsGiven() {
         val expectedUsers = setOf(
             User("567", "Joe Bloggs"),
             User("123", "Henrique Cocito"),
         )
 
         val text = "Thanks to @[Joe Bloggs](567) and @[Henrique Cocito](123)"
+        val response = getUserFromTextUseCase.execute(text)
+
+        assertEquals(response, expectedUsers)
+    }
+
+    @Test
+    fun shouldReturnAnEmptySet_whenTextWithNoUserTagIsGiven() {
+        val expectedUsers = setOf<User>()
+
+        val text = "Thanks to Joe Bloggs and Henrique Cocito"
+        val response = getUserFromTextUseCase.execute(text)
+
+        assertEquals(response, expectedUsers)
+    }
+
+    @Test
+    fun shouldReturnAnEmptySet_whenTextWithPartOfUserTagIsGiven() {
+        val expectedUsers = setOf<User>()
+
+        val text = "Thanks to [Joe Bloggs](123) and @[Henrique Cocito]"
+        val response = getUserFromTextUseCase.execute(text)
+
+        assertEquals(response, expectedUsers)
+    }
+
+    @Test
+    fun shouldReturnAnEmptySet_whenTextWithEmptyUserTagIsGiven() {
+        val expectedUsers = setOf<User>()
+
+        val text = "Thanks to Joe @[]() and @[]()"
         val response = getUserFromTextUseCase.execute(text)
 
         assertEquals(response, expectedUsers)
