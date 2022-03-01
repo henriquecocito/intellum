250c675a4729f568ff861b2d6efbec919176a09c
Henrique Cocito <henriquecocito@gmail.com>
2022-03-01 19:06:28 -0300

    chore: add missing files

--- src/main/kotlin/com/henriquecocito/intellum/domain/useCases/GetUsersFromTextUseCase.kt
+++
@@ -1,6 +0,0 @@
-package com.henriquecocito.intellum.domain.useCases
-
-import com.henriquecocito.intellum.base.UseCase
-import com.henriquecocito.intellum.entities.User
-
-interface GetUsersFromTextUseCase : UseCase<String, Set<User>>
